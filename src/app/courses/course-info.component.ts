fa61ebc3d338f4316bb9e8caf130a170e6facf4f
Lukas Leucz <leucz93@gmail.com>
2020-06-05 12:53:10 -0300

    Ativando Rotas através de Links

---
+++ src/app/courses/course-info.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+    templateUrl: './course-info.component.html'
+
+})
+export class CourseInfoComponent implements OnInit{
+
+    courseId: number;
+
+    constructor(private ActivateRoute: ActivatedRoute) { }
+    ngOnInit(): void{
+        this.courseId = +this.ActivateRoute.snapshot.paramMap.get('id'); //informado + para indicar que retorne number
+
+    }
+
+}
